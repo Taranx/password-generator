e245a842fe4fde964ef38dd1181c7275b13bd52a
Luis T <luistarantini@gmail.com>
2022-09-17 23:14:35 -0500

    pushing to repo

---
+++ README.md
@@ -0,0 +1,49 @@
+# # Password Generator
+
+
+## Description
+
+This is a web deployed password generator. Parameters are input by the user in order to generate a password between 8 and 128 characters long. The user will be presented a series of prompts in order to select different parameters between lower case letters, upper case letters, numbers and special characters. 
+
+## Website Screenshot
+
+<img src="./images/Screenshot_1.jpg">
+
+## Installation
+ 
+No installation needed. 
+
+## Usage 
+
+Click the Generate Password button and follow the instructions. 
+
+## License 
+
+MIT License
+
+Copyright (c) 2022 Luis T
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+## Credits
+
+N/A
+
+## Deployed website URL: 
---
+++ script.js
@@ -0,0 +1,93 @@
+
+//Assignment Code and event listener.
+
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// Arrays
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetters= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~", "@", "{", "}", "[", "]"];
+
+// Variable Declaration 
+
+var chooseLength = "";
+var chooseUpperCase;
+var chooseLowerCase;
+var chooseNumbers;
+var chooseSpecialCharacter;
+
+
+// Prompt to confirm how many characters the user would like in their password
+
+function generatePassword() {
+  var chooseLength = (prompt("Please choose a number between 8 and 128 for the lenght of your password"));
+
+  // Loop if answer is outside the parameters 
+
+  while(chooseLength <= 7 || chooseLength >= 129 ) {
+      alert("You must choose a number between 8 and 128 to determine the length of your password");
+      var chooseLength = (prompt("Please choose a number between 8 and 128 for the lenght of your password"));
+      } 
+
+    
+    // Password parameters 
+
+
+    var chooseLowerCase = confirm("Would you like lower case letters in your password? Click OK to confirm");
+    var chooseUpperCase = confirm("Would you like upper case letters in your password? Click OK to confirm");
+    var chooseNumbers = confirm("Would you like numbers in your password? Click OK to confirm");  
+    var chooseSpecialCharacter = confirm("Would you like special characters in your password? Click OK to confirm");
+
+      // Loop if user selects cancel on all 4 parameters
+
+      while(chooseUpperCase  === false && chooseLowerCase === false && chooseNumbers === false && chooseSpecialCharacter === false) {
+        alert("You must choose at least one parameter");
+        var chooseLowerCase = confirm("Would you like lower case letters in your password? Click OK to confirm");
+        var chooseUpperCase = confirm("Would you like upper case letters in your password? Click OK to confirm");
+        var chooseNumbers = confirm("Would you like numbers in your password? Click OK to confirm");  
+        var chooseSpecialCharacter = confirm("Would you like special characters in your password? Click OK to confirm"); 
+    } 
+
+      // Assign an action to the password parameters
+
+      var passwordCharacters = []
+      
+
+    if (chooseNumbers) {
+      passwordCharacters = passwordCharacters.concat(numbers)
+    }
+      
+    if (chooseLowerCase) {
+      passwordCharacters = passwordCharacters.concat(lowerLetters)
+    }
+
+    if (chooseUpperCase) {
+      passwordCharacters = passwordCharacters.concat(upperLetters)
+    }
+
+    if (chooseSpecialCharacter) {
+      passwordCharacters = passwordCharacters.concat(specialChar)
+    }
+
+      console.log(passwordCharacters)
+
+      // For loop to fill empty string from values from the arrays to use for the random generated password.
+
+      var randomPassword = ""
+      
+      for (var i = 0; i < chooseLength; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+      }
+      return randomPassword;
+}
+
+// Write password to the #password input
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
